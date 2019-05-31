cf2e7ec0c481553f34e37fa5f2b583a65e3267b9
William Pyburn <texas.william@gmail.com>
2019-05-30 19:52:02 -0500

    Cleaning up osx-gnubinman.bash.

--- src/bash_profile.d/osx-gnubinman.bash
+++ src/bash_profile.d/osx-gnubinman.bash
@@ -1,9 +1,5 @@
 [ "`uname -s`" = Darwin ] || return 0
 
-for gnubin in /usr/local/opt/*/libexec/gnubin; do
-    export PATH="$gnubin:$PATH"
-done
-
-for gnuman in /usr/local/opt/*/libexec/gnuman; do
-    export MANPATH="$gnuman:${MANPATH:-"`man --path`"}"
-done
+for gnubin in /usr/local/opt/*/libexec/gnubin; do PATH="$gnubin:$PATH"; done
+for gnuman in /usr/local/opt/*/libexec/gnuman; do MANPATH="$gnuman:$MANPATH"; done
+export MANPATH PATH
