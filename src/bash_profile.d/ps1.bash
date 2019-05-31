470b048c199b2a8cf3bea41983e4597c88f94541
William Pyburn <texas.william@gmail.com>
2019-05-30 19:55:22 -0500

    ps1.bash overhaul

--- src/bash_profile.d/ps1.bash
+++ src/bash_profile.d/ps1.bash
@@ -1,7 +1,14 @@
-if [ "$CLICOLOR" = 1 ]; then
-    PS1='\[\e[0;34m\]\u\[\e[0m\]@\[\e[36m\]\h\[\e[0m\]:\[\e[37m\]\w\[\e[31m\]$(__git_ps1 %s)\[\e[0m\]?\[\e[$(($? == 0 ? 32 : 31))m\]$?\[\e[0m\]\$ '
+if whether tput; then
+    sgr0="$(tput sgr0)"
+    bold="$(tput bold)"
+    [ "$CLICOLOR" = 1 ] && for ((i = 0; i <= 7; ++i)); do setaf[$i]="$(tput setaf $i)"; done
 else
-    PS1='\[\e[0m\]\u@\h:\w$(__git_ps1 %s)?$?\$ '
+    sgr0="\e[m"
+    bold="\e[1m"
+    [ "$CLICOLOR" = 1 ] && for ((i = 0; i <= 7; ++i)); do setaf[$i]="\e[3${i}m"; done
 fi
 
-export PS1
+PROMPT_COMMAND='[ $? = 0 ] && PS1_RETVALCOLOR='${setaf[2]@Q}' || PS1_RETVALCOLOR='${setaf[1]@Q}
+PS1='\['$sgr0$bold${setaf[4]}'\]\u\['$sgr0'\]@\['$bold${setaf[6]}'\]\h\['$sgr0'\]:\['$bold${setaf[7]}'\]\w\['${setaf[1]}'\]$(__git_ps1 %s)\['$sgr0'\]?\['$bold'$PS1_RETVALCOLOR\]$?\['$sgr0'\]\$ '
+
+export PROMPT_COMMAND PS1
