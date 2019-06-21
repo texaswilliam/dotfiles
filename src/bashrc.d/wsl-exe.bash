7f4d27f0c9384fc86375fcf253670183eaf97485
William Pyburn <texas.william@gmail.com>
2019-06-21 11:43:04 -0500

    .bashrc.d/wsl-exe.bash

---
+++ src/bashrc.d/wsl-exe.bash
@@ -0,0 +1,15 @@
+[[ "`uname -r`" =~ Microsoft$ ]] || return 0
+
+command_not_found_handle () {
+    local cmd="$1"
+    shift 1
+
+    if ! { hash "$cmd.exe" && hash -p "$(hash -t "$cmd.exe")" "$cmd"; } &> /dev/null; then
+        echo "$0: $cmd: command not found"
+        echo "$0: $cmd.exe: command not found"
+        return 127
+    fi
+
+    eval "$cmd" "$@"
+}
+export -f command_not_found_handle
