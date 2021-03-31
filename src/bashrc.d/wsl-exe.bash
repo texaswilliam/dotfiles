9295be15da3852db13392c3981842d796be47339
William Pyburn <texas.william@gmail.com>
2021-03-30 21:50:17 -0500

    Adding .bat to wsl-exe.

--- src/bashrc.d/wsl-exe.bash
+++ src/bashrc.d/wsl-exe.bash
@@ -4,9 +4,11 @@ command_not_found_handle () {
     local cmd="$1"
     shift 1
 
-    if ! { hash "$cmd.exe" && hash -p "$(hash -t "$cmd.exe")" "$cmd"; } &> /dev/null; then
+    if ! { hash "$cmd.exe" && hash -p "$(hash -t "$cmd.exe")" "$cmd" &&
+            hash "$cmd.bat" && hash -p "$(hash -t "$cmd.bat")" "$cmd"; } &> /dev/null; then
         echo "$0: $cmd: command not found"
         echo "$0: $cmd.exe: command not found"
+        echo "$0: $cmd.bat: command not found"
         return 127
     fi
 
